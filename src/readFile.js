6db0d8da2635a57f165b746d504ceb78099736ef
fraolb <Fraolbereket@gmail.com>
2023-05-29 10:30:42 +0300

    initial

---
+++ src/readFile.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+// import the lodash library
+const lodash = require("lodash");
+
+// Read the file data and return the data in the resolved Promise
+const read = (fileName) => {
+  return new Promise((resolve, reject) => {});
+};
+// Define a function to Convert the file content to upper case and return the result in the resolved Promise
+const convertToUpperCase = (fileContents) => {
+  return new Promise((resolve, reject) => {});
+};
+// Define a function to read and convert the file contents, use the then and catch blocks here
+const readAndConvertFileContents = (fileName, cb) => {};
+
+module.exports = {
+  readAndConvertFileContents,
+};
